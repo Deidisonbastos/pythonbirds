dedf15c2860728587000ec5b12b761379303e07c
dbpereira <deidsonbastos@gmail.com>
2019-01-14 15:40:41 -0200

    Lista, Range...

---
+++ Aula.py
@@ -0,0 +1,708 @@
+C:\Users\dbpereira\AppData\Local\Programs\Python\Python37\python.exe
+"C:\Program Files\JetBrains\PyCharm Community Edition 2018.3.3\helpers\pydev\pydevconsole.py" - -mode = client - -port = 64664
+import sys;
+
+print('Python %s on %s' % (sys.version, sys.platform))
+sys.path.extend(
+    ['C:\\Users\\dbpereira\\PycharmProjects\\pythonbirds', 'C:/Users/dbpereira/PycharmProjects/pythonbirds'])
+PyDev
+console: starting.
+Python
+3.7
+.2(tags / v3
+.7
+.2: 9
+a3ffc0492, Dec
+23
+2018, 23: 0
+9: 28) [MSC v.1916 64 bit(AMD64)]
+on
+win32
+list = Deidison
+Traceback(most
+recent
+call
+last):
+File
+"<input>", line
+1, in < module >
+NameError: name
+'Deidison' is not defined
+deidison = List
+Traceback(most
+recent
+call
+last):
+File
+"<input>", line
+1, in < module >
+NameError: name
+'List' is not defined
+list
+<
+
+class 'list'>
+
+
+list = 'deidison'
+type(deidison)
+Traceback(most
+recent
+call
+last):
+File
+"<input>", line
+1, in < module >
+NameError: name
+'deidison' is not defined
+list = [1, 2, 3, 4, 5]
+list
+[1, 2, 3, 4, 5]
+list = [a, b, c]
+Traceback(most
+recent
+call
+last):
+File
+"<input>", line
+1, in < module >
+NameError: name
+'a' is not defined
+list = [a, b, c]
+Traceback(most
+recent
+call
+last):
+File
+"<input>", line
+1, in < module >
+NameError: name
+'a' is not defined
+list
+[1, 2, 3, 4, 5]
+for 1(range(a, b, c, d, e)):
+    a = 1:
+    b = 2:
+    c = 3:
+    d = 4:
+    e = 5:
+
+    File
+    "<input>", line
+    1
+    for 1(range(a, b, c, d, e)):
+        ^
+        SyntaxError: invalid
+        syntax
+        for 1(range(a, b, c, d, e)):
+            a = 1:
+            b = 2:
+            c = 3:
+            d = 4:
+            e = 5:
+            print('a')
+
+        File
+        "<input>", line
+        1
+        for 1(range(a, b, c, d, e)):
+            ^
+            SyntaxError: invalid
+            syntax
+            print('1')
+            1
+            print('a')
+            a
+            lst = [1, 2, 3, 4, 5]
+            letras = [a, b, c, d, e]
+            Traceback(most
+            recent
+            call
+            last):
+            File
+            "<input>", line
+            1, in < module >
+            NameError: name
+            'a' is not defined
+            letras = (a, b, c, d, e)
+            Traceback(most
+            recent
+            call
+            last):
+            File
+            "<input>", line
+            1, in < module >
+            NameError: name
+            'a' is not defined
+            letras = a, b, c, d, e
+            Traceback(most
+            recent
+            call
+            last):
+            File
+            "<input>", line
+            1, in < module >
+            NameError: name
+            'a' is not defined
+            letras = a, b, c, d, e
+            Traceback(most
+            recent
+            call
+            last):
+            File
+            "<input>", line
+            1, in < module >
+            NameError: name
+            'a' is not defined
+            lst
+            [1, 2, 3, 4, 5]
+            a = 1
+            b = 2
+            c = 3
+            d = 4
+            5 = e
+            File
+            "<input>", line
+            5
+            SyntaxError: can
+            't assign to literal
+            a = 1
+            b = 2
+            c = 3
+            d = 4
+            e = 5
+            for i string(range(lst)):
+                print('a,b,c,d,e')
+
+            File
+            "<input>", line
+            1
+            for i string(range(lst)):
+                ^
+                SyntaxError: invalid
+                syntax
+                listaletra = ('a', 'b', 'c', 'd', 'e')
+                listanum = list(range(1, 10, 2))
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                1, in < module >
+                TypeError: 'list'
+                object is not callable
+                listanum = list(range(1, 10, 2))
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                1, in < module >
+                TypeError: 'list'
+                object is not callable
+                listanumero = list(range(1, 10, 2))
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                1, in < module >
+                TypeError: 'list'
+                object is not callable
+                ltsnumero = lst(range(1, 10, 2))
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                1, in < module >
+                TypeError: 'list'
+                object is not callable
+                listaabc = ('a', 'b', 'c', 'd', 'e')
+                listanumero = list(range(1, 10, 2))
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                1, in < module >
+                TypeError: 'list'
+                object is not callable
+                letras = 'abcdefg'
+                list = range(1, 10, 2)
+                for i in range(len(list)):
+                    print(letras[i], lista[i])
+
+                Traceback(most
+                recent
+                call
+                last):
+                File
+                "<input>", line
+                2, in < module >
+                NameError: name
+                'lista' is not defined
+                for i in range(len(list)):
+                    print(letras[i], lista[i]
+
+                    for i in range(len(list)):
+                        print(letras[i], lista[i])
+
+                    File
+                    "<input>", line
+                    11
+                    for i in range(len(list)):
+                        ^
+                    SyntaxError: invalid
+                    syntax
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(1, 10, 2)
+                    lis
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    1, in < module >
+                            NameError: name
+                    'lis' is not defined
+                    list
+                    range(1, 10, 2)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    a
+                    1
+                    b
+                    3
+                    c
+                    5
+                    d
+                    7
+                    e
+                    9
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(1, 10, 2)
+                    list
+                    range(1, 10, 2)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    a
+                    1
+                    b
+                    3
+                    c
+                    5
+                    d
+                    7
+                    e
+                    9
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(1, 30, 2)
+                    range(1, 30, 2)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    8, in < module >
+                            IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    a
+                    1
+                    b
+                    3
+                    c
+                    5
+                    d
+                    7
+                    e
+                    9
+                    f
+                    11
+                    g
+                    13
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(1, 30, 2)
+                    range(1, 30, 2)
+                    dir
+                    i in range(len(list)):
+                    print(letras[i], list[i])
+                    File
+                    "<input>", line
+                    7
+                    dir
+                    i in range(len(list)):
+                    ^
+                    SyntaxError: invalid
+                    syntax
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    a
+                    0
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    b
+                    1
+                    File
+                    "<input>", line
+                    7, in < module >
+                            c
+                    2
+                    IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    d
+                    3
+                    e
+                    4
+                    f
+                    5
+                    g
+                    6
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30) * 2
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    5, in < module >
+                            TypeError: unsupported
+                    operand
+                    type(s)
+                    for *: 'range' and 'int'
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(1, 10, 2)
+                    list
+                    range(1, 10, 2)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    a
+                    1
+                    b
+                    3
+                    c
+                    5
+                    d
+                    7
+                    e
+                    9
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    list
+                    range(30) * 2
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    7, in < module >
+                            TypeError: unsupported
+                    operand
+                    type(s)
+                    for *: 'range' and 'int'
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30) * 2
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    5, in < module >
+                            TypeError: unsupported
+                    operand
+                    type(s)
+                    for *: 'range' and 'int'
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list)):
+                        print(letras[i], list[i]) * 2
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    7, in < module >
+                            a
+                    0
+                    TypeError: unsupported
+                    operand
+                    type(s)
+                    for *: 'NoneType' and 'int'
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list) * 2):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    a
+                    0
+                    File
+                    "<input>", line
+                    7, in < module >
+                            b
+                    1
+                    IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    c
+                    2
+                    d
+                    3
+                    e
+                    4
+                    f
+                    5
+                    g
+                    6
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    7, in < module >
+                            IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    a
+                    0
+                    b
+                    1
+                    c
+                    2
+                    d
+                    3
+                    e
+                    4
+                    f
+                    5
+                    g
+                    6
+                    letras
+                    'abcdefg'
+                    list
+                    range(0, 30)
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    2, in < module >
+                            IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    a
+                    0
+                    b
+                    1
+                    c
+                    2
+                    d
+                    3
+                    e
+                    4
+                    f
+                    5
+                    g
+                    6
+                    for i in range(len(list)):
+                        print(letras[i], list[i]) ** 2
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    2, in < module >
+                            TypeError: unsupported
+                    operand
+                    type(s)
+                    for ** or pow(): 'NoneType' and 'int'
+                    a
+                    0
+                    for i in range(len(list)):
+                        print(letras[i], list[i]) ** 2
+
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    a
+                    0
+                    File
+                    "<input>", line
+                    2, in < module >
+                            TypeError: unsupported
+                    operand
+                    type(s)
+                    for ** or pow(): 'NoneType' and 'int'
+                    for i in enumarate(list)):
+                        print(letras[i], sep
+                        ' - ')
+
+                    File
+                    "<input>", line
+                    1
+                    for i in enumarate(list)):
+                        ^
+                    SyntaxError: invalid
+                    syntax
+                    for i in range(len(list)):
+                        print(letras[i], list[i])
+
+                    a
+                    0
+                    b
+                    1
+                    c
+                    2
+                    d
+                    3
+                    e
+                    4
+                    f
+                    5
+                    g
+                    6
+                    Traceback(most
+                    recent
+                    call
+                    last):
+                    File
+                    "<input>", line
+                    2, in < module >
+                            IndexError: string
+                    index
+                    out
+                    of
+                    range
+                    for i in range(len(list)):
+                        1 = 1 % lang(letras)
+                    print(letras[i], list[i])
+
+                    File
+                    "<input>", line
+                    2
+                    SyntaxError: can
+                    't assign to literal
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list)):
+                        1 = 1 % lang(letras)
+                    print(letras[i], list[i])
+
+                    File
+                    "<input>", line
+                    7
+                    SyntaxError: can
+                    't assign to literal
+                    letras = 'abcdefg'
+                    letras = 'abcdefg'
+                    letras
+                    'abcdefg'
+                    list = range(30)
+                    for i in range(len(list)):
+                        1 = 1 % len(letras)
+                    print(letras[i], list[i])
+
+                    File
+                    "<input>", line
+                    7
+                    SyntaxError: can
+                    't assign to literal
